e93d101e994efa9685065a6a970a893a5e9be629
Thouhid98 <thouhidhussain98@gmail.com>
2023-12-21 00:54:26 +0600

    Navbar, Header, footer done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Mainlayout from './Layout/Mainlayout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Mainlayout></Mainlayout>,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
